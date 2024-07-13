ff34f6c58fc035c80ffe9151b84abb328a732927
noahblob <noahhagardent@gmail.com>
2024-07-13 19:57:34 +1200

    Added text database, fixed issue with categories not showing

--- client/app.py
+++ client/app.py
@@ -1,4 +1,3 @@
-import sqlite3
 from flask import Flask, render_template, request, jsonify, session, Response
 import cv2
 import mediapipe as mp
@@ -7,8 +6,6 @@ import numpy as np
 import warnings
 from flask_socketio import SocketIO, emit
 import base64
-
-import database
 
 app = Flask(__name__, static_url_path='/static')
 socketio = SocketIO(app)
@@ -19,17 +16,59 @@ app.secret_key = 'your_secret_key'
 warnings.filterwarnings("ignore", category=UserWarning, message=".*GetPrototype.*")
 
 words = {
-    "alphabet": ["A", "B", "C"],
-    "numbers": ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10"],
-    "days": ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"],
-    "terms": ["Hello", "Goodbye", "Please", "Thank you", "Sorry", "Yes", "No"],
-    "nature": ["Mountain", "Water", "Te reo"]
+    "greetings": {
+        "Hello": "Kia ora",
+        "Goodbye": "Haere rā",
+        "Thank you": "Ngā mihi"
+    },
+    "numbers": {
+        "Zero": "Kore",
+        "One": "Tahi",
+        "Two": "Rua",
+        "Three": "Toru",
+        "Four": "Whā",
+        "Five": "Rima",
+        "Six": "Ono",
+        "Seven": "Whitu",
+        "Eight": "Waru",
+        "Nine": "Iwa"
+    },
+    "days": {
+        "Monday": "Rāhina",
+        "Tuesday": "Rātū",
+        "Wednesday": "Rāapa",
+        "Thursday": "Rāpare",
+        "Friday": "Rāmere",
+        "Saturday": "Rāhoroi",
+        "Sunday": "Rātapu"
+    },
+    "months": {
+        "January": "Kohitātea",
+        "February": "Hui-tanguru",
+        "March": "Poutū-te-rangi",
+        "April": "Paenga-whāwhā",
+        "May": "Haratua",
+        "June": "Pipiri",
+        "July": "Hōngongoi",
+        "August": "Here-turi-kōkā",
+        "September": "Mahuru",
+        "October": "Whiringa-ā-nuku",
+        "November": "Whiringa-ā-rangi",
+        "December": "Hakihea"
+    },
+    "terms": {
+        "Welcome": "Nau mai",
+        "Please": "Tēnā koa",
+        "Yes": "Āe",
+        "No": "Kāo",
+        "Excuse me": "Aroha mai",
+        "Family": "Whānau",
+        "Friend": "Hoa",
+        "House": "Whare",
+        "Food": "Kai",
+        "Water": "Wai"
+    }
 }
-
-connection, cursor = database.connectDB()
-database.setUpWordsTable(connection, cursor, words)
-database.printTable(cursor, "words")
-database.closeDB(connection)
 
 # Load the model
 model_dict = pickle.load(open('model.pickle', 'rb'))
@@ -49,6 +88,18 @@ currentTime = 0
 last_prediction = None
 gesture_start_time = None
 gesture_held = False
+
+def save_gesture(gesture):
+    filename = 'database.txt'
+    try:
+        with open(filename, 'a+') as file:
+            file.seek(0)
+            existing_gestures = file.read().splitlines()
+            if gesture not in existing_gestures:
+                file.write(gesture + '\n')
+    except FileNotFoundError:
+        with open(filename, 'w') as file:
+            file.write(gesture + '\n')
 
 def generate_frames():
     global prediction, last_prediction, gesture_start_time, gesture_held, currentTime
@@ -83,13 +134,14 @@ def generate_frames():
                 if len(data_aux) == 84:
                     prediction = model.predict([np.asarray(data_aux)])
                     predicted_gesture = prediction[0]
-                    # print(f"Predicted Gesture: {prediction}")
+                    # print(f"Predicted Gesture: {predicted_gesture}")
 
                     if predicted_gesture == last_prediction:
                         if gesture_start_time is None:
                             gesture_start_time = currentTime;
                         elif currentTime - gesture_start_time >= 30:
                             gesture_held = True
+                            save_gesture(predicted_gesture)
                     else:
                         last_prediction = predicted_gesture
                         gesture_start_time = None
@@ -130,11 +182,11 @@ def pokedex():
     seen_words = session.get('seen_words', [])
     return render_template('pokedex.html', words=words, seen_words=seen_words)
 
-# @app.route('/category/<category>')
-# def category(category):
-#     category_words = words.get(category, {})
-#     seen_words = session.get('seen_words', [])
-#     return render_template('category.html', category=category.capitalize(), words=category_words, seen_words=seen_words)
+@app.route('/category/<category>')
+def category(category):
+    category_words = words.get(category, {})
+    seen_words = session.get('seen_words', [])
+    return render_template('category.html', category=category.capitalize(), words=category_words, seen_words=seen_words)
 
 if __name__ == '__main__':
     app.run(debug=True)
